a27b954ab2d965c4f624345d7eb5294a085db8b9
Loren <luo_sd@yeah.net>
2020-05-06 21:37:24 +0800

    hello world

--- pages/userprofiles/profile.js
+++ pages/userprofiles/profile.js
@@ -1,80 +1,66 @@
-//index.js
-//获取应用实例
-const app = getApp()
+// pages/userprofiles/profile.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    "title":"首页"
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.showLoading({
-      title: '数据加载中...',
-    })
-    wx.request({
-      url: 'http://localhost:1601/api/BookCategory',
-      method:'post',
-      data:{},
-      success:res=>{
-        console.log(res.data);
-        wx.hideLoading();
-      }
-    })
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
